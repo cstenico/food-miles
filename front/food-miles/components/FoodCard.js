3f4250b2cfe0b430efef31db3140bbe2a3435eca
carolesj <carolinejesuino.ns@gmail.com>
2019-06-10 00:19:08 -0300

    Add style to component

--- front/food-miles/components/FoodCard.js
+++ front/food-miles/components/FoodCard.js
@@ -1,44 +1,120 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Card, CardItem, Thumbnail, Text, Button, Icon, Left, Right, Body } from 'native-base';
+import { View, Image, ImageBackground, StyleSheet, TouchableOpacity} from 'react-native';
+import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Right, Body } from 'native-base';
 
 export default class FoodCard extends Component {
   render() {
     return (
-      <Card style={{flex: 0}}>
+      <Card style={styles.card}>
         <CardItem>
           <Left>
-            <Text>{this.props.item.name}</Text>
+            <Text style={styles.textTitle}>{this.props.item.name}</Text>
           </Left>
         </CardItem>
         <CardItem>
           <Left>
             <Thumbnail source={{uri: this.props.shop.image}} />
             <Body>
-              <Text>{this.props.shop.name}</Text>
-              <Text note>{this.props.shop.address}</Text>
+              <Text style={styles.textShopName}>{this.props.shop.name}</Text>
+              <Text style={styles.textAddress}>{this.props.shop.address}</Text>
             </Body>
           </Left>
           <Right>
-            <Text>{this.props.item.price}</Text>
+            <Button style={styles.priceButton}>
+              <Text style={styles.textPrice}>{this.props.item.price}</Text>
+            </Button>
           </Right>
         </CardItem>
         <CardItem>
           <Body>
-            <Image source={{uri: this.props.item.image}} style={{height: 200, width: 200, flex: 1}}/>
-            <Text>
+            <Right>
+            <Image
+              source={{uri: this.props.item.image}}
+              style={styles.image}/>
+            </Right>
+            <Text style={styles.textBody}>
               {this.props.item.description}
             </Text>
           </Body>
         </CardItem>
         <CardItem>
-          <Left>
-            <Button textStyle={{color: '#87838B'}}>
-              <Text>Chat</Text>
+            <Button style={styles.chatButton}>
+              <Text style={styles.textButton}>Contato</Text>
             </Button>
-          </Left>
         </CardItem>
       </Card>
     )
   }
 }
+
+var styles = StyleSheet.create({
+  card:{
+    backgroundColor: '#7FA99B',
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#FBF2D5'
+  },
+  chatButton:{
+    backgroundColor: '#FBF2D5',
+    marginTop:10,
+    paddingTop:15,
+    paddingBottom:15,
+    marginLeft:15,
+    marginRight:15,
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#FBF2D5'
+  },
+  priceButton:{
+    backgroundColor: '#394A51',
+    marginTop:10,
+    paddingTop:15,
+    paddingBottom:15,
+    marginLeft:25,
+    marginRight:25,
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#394A51'
+  },
+  textPrice: {
+    fontFamily: 'Arvo',
+    color: '#FDC57B',
+    fontSize: 16
+  },
+  textTitle: {
+    fontFamily: 'Arvo',
+    color: '#FBF2D5',
+    fontSize: 24
+  },
+  textButton: {
+    fontFamily: 'Arvo',
+    textAlign: 'center',
+    color: '#394A51'
+  },
+  textBody: {
+    fontFamily: 'Arvo',
+    color: '#FBF2D5'
+  },
+  textAddress: {
+    fontFamily: 'Arvo',
+    color: '#394A51',
+    fontSize: 14
+  },
+  textShopName :{
+    fontFamily: 'Arvo',
+    color: '#394A51',
+    fontSize: 18
+  },
+  link:{
+    fontFamily: 'Arvo',
+    textAlign: 'center',
+    color:'#7fa99b'
+  },
+  image: {
+    height: 200,
+    width: 200,
+    flex: 1
+  }
+});
